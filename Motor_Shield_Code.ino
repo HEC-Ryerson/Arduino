c6d142d88093bf064ab6b4db292600f20c4ebbc9
HEC-Ryerson <sadeed.bari@ryerson.ca>
2018-01-24 23:00:37 -0500

    Add files via upload

---
+++ Motor_Shield_Code.ino
@@ -0,0 +1,37 @@
+int E2 = 11;
+int M2 = 13; 
+const int xPin = 2;     // X output of the accelerometer
+const int yPin = 3;     // Y output of the accelerometer
+
+void setup()
+{
+  Serial.begin (9600);
+ pinMode(xPin, INPUT);
+ pinMode(yPin, INPUT);
+ pinMode(M2, OUTPUT);
+}
+void loop()
+{
+  int pulseX, pulseY;
+  pulseX = pulseIn(xPin, HIGH);
+  pulseY = pulseIn(yPin, HIGH);
+  
+ if (pulseY >= 5000){
+ // FORWARD 
+ analogWrite(E2, 255);
+ digitalWrite(M2, HIGH); 
+ Serial.println(pulseY);
+ }
+
+else {
+ // REVERSE
+ analogWrite(E2, 255);
+ digitalWrite(M2, LOW);
+ Serial.println(pulseY);
+}
+// // STOP
+// analogWrite(E2, 0);
+// digitalWrite(M2, LOW); 
+// delay(1000);
+ }
+
